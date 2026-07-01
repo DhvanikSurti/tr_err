90b067fc30fb5fa6f814d70451588a2b020d8546
DhvanikSurti <dhvaniksurti35@gmail.com>
2026-07-01 17:47:33 +0000

    descending sort

--- 01jul.c
+++ 01jul.c
@@ -1,21 +1,45 @@
 //ayo, this is just simple a insertion sort 
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main(){
+
+// 	int a[3]={3,2,1};
+// 	int n =sizeof(a) / sizeof(a[0]);
+// 	for (int i=1;i<n;i++){
+// 		int temp = a[i];
+// 		int j = i-1;
+
+// 		while(j>=0 && a[j]>temp){
+// 			a[j+1]= a[j];
+// 			j--;
+// 		}
+// 		a[j+1] = temp;
+// 	}
+// 	printf("Data is sorted \n");
+// 	for(int k =0; k < n; k++){
+// 		printf("%d",a[k]);
+// 	}
+// 	printf("\n");
+// }
+
+// Descending order insertion sort
+
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-
-	int a[3]={3,2,1};
+int main(){		
+	int a[3]={1,2,3};
 	int n =sizeof(a) / sizeof(a[0]);
 	for (int i=1;i<n;i++){
 		int temp = a[i];
 		int j = i-1;
 
-		while(j>=0 && a[j]>temp){
+		while(j>=0 && a[j]<temp){
 			a[j+1]= a[j];
 			j--;
 		}
 		a[j+1] = temp;
 	}
-	printf("Data is sorted \n");
+	printf("Data is sorted in descending order \n");
 	for(int k =0; k < n; k++){
 		printf("%d",a[k]);
 	}
