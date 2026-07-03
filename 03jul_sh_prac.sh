80ce0c4d7d200f5735147ede26f99a5d77924e98
DhvanikSurti <dhvaniksurti35@gmail.com>
2026-07-03 16:03:19 +0000

    shell scripting practice - loops and variables

---
+++ 03jul_sh_prac.sh
@@ -0,0 +1,43 @@
+#ayo, today's covered topics are shell operators , 
+
+#!/bin/bash
+#
+# echo "enter var1 value"
+# read var
+# echo "enter var2 value"
+# read var2
+# var3=$(expr $var + $var2)
+# echo $var3
+
+# while and until loops
+# a=0
+# while [ $a -lt 10 ] 
+# do 
+#     echo $a
+#     a=$(expr $a + 1)
+# done
+
+# until [ ! $a -lt 5 ]
+# do 
+#     echo $a
+#     a=$(expr $a + 1)
+
+# done
+
+
+#nested loop
+# a=0
+# while [ $a -lt 10 ]
+# do
+#     b=$a
+#     while [ $b -gt 0 ]
+#     do 
+#         echo -n $b
+#         b=$(expr $b - 1)
+#     done
+#     echo 
+#     a=$(expr $a + 1)
+# done
+
+
+#loop control 
