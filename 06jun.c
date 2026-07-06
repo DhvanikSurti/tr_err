2277d1c2576788e7d9f6a48703ec145bc6429d73
DhvanikSurti <dhvaniksurti35@gmail.com>
2026-07-06 16:26:01 +0000

    strut pointer members

--- 06jun.c
+++ 06jun.c
@@ -1,16 +1,46 @@
-#include<stdio.h>
-struct s{
-        char name[20];
-        int age;
-    };
+// #include<stdio.h>
+// struct s{
+//         char name[20];
+//         int age;
+//     };
 
-int main()
-{
+// int main()
+// {
+//     struct s s;
+//     printf("Size of struct is %zu bytes\n", sizeof(struct s));
+//     printf("Enter name: ");
+//     scanf("%s", s.name);
+//     printf("Enter age: ");
+//     scanf("%d", &s.age);
+//     printf("Stored in the struct is: Name: %s, Age: %d\n", s.name, s.age);
+// }
+
+
+#include<stdio.h>
+#include<stdlib.h>
+struct s{
+    char *name;
+    int *age;
+};
+int main (){
     struct s s;
+    int new;
+    s.name=(char *)malloc(20*sizeof(char));
+    s.age=(int *)malloc(sizeof(int));
+
     printf("Size of struct is %zu bytes\n", sizeof(struct s));
     printf("Enter name: ");
-    scanf("%s", s.name);
+    scanf("%s", s.name);  
     printf("Enter age: ");
-    scanf("%d", &s.age);
-    printf("Stored in the struct is: Name: %s, Age: %d\n", s.name, s.age);
+    scanf("%d", s.age);  
+
+    printf("Addree of stuct members are  %p and %p\n",&s.name,&s.age);
+
+    printf("Dereference operator(address of that stored value,malloc)  value is %s and %d\n",s.name,*s.age);
+
+    printf("before free pointers address is : name=%p, age=%p\n", s.name, s.age); 
+
+    printf("Difference beyween pointers is : %ld\n", (char *)s.age - (char *)s.name); // Print difference
+    free(s.name);
+    free(s.age);    
 }
