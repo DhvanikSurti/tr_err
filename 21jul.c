7c104ba802296e7be1f5174f30569fd44982cc97
DhvanikSurti <230488101+DhvanikSurti@users.noreply.github.com>
2026-07-21 16:11:28 +0000

    pipe code

---
+++ 21jul.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(){
+
+   int fd[2];
+   char w[7]="hello";
+   char r[7];
+   pipe(fd);
+   if(fork()==0){
+      printf("child process runs \n");
+      close(fd[0]);
+      write(fd[1],w,strlen(w)+1);
+      printf("child has writen to pipe\n");
+      close(fd[1]);
+   }
+   else{
+      printf("parent process run \n");
+      close(fd[1]);
+      read(fd[0],r,sizeof(r));
+      printf("parent read : %s\n",r);
+
+   }
+   return 0;
+}
