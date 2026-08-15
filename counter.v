99b251d89d8ad655fde26ed0f03944dcc716c32e
DhvanikSurti <dhvaniksurti35@gmail.com>
2026-08-15 09:45:48 +0000

    4 bit loadable counter with normal 1 bit counting

--- counter.v
+++ counter.v
@@ -1,15 +1,17 @@
 module counter(
-    input clk,enb,rst,
-    output reg q
+    input clk,load,rst,
+    input [3:0] din,
+    output reg [3:0]count
 );
 
 always@(negedge clk)begin
     if(rst)begin
-        q<=1'b0;
+        count<=4'b0000;
     end
-    else if(enb)begin
-        q <=~q;
+    else if(load)begin
+        count<=din;
     end
+    else count <= count+1'b1;
 end
 endmodule
 
@@ -17,26 +19,31 @@ endmodule
 `timescale 1ps/1ps
 module tb;
 
-reg clk,enb,rst;
-wire q;
+reg clk,load,rst;
+reg [3:0]din;
+wire [3:0]count;
 
 counter dut(
-    clk,enb,rst,q
-);
+    clk,load,rst,din,count
+    );
 initial begin
-    {clk,enb,rst}=0;
+    {clk,load,rst,din}=0;
 end
 always #5 clk = ~clk;
 initial begin
     $dumpfile("counter.vcd");
     $dumpvars(0,tb);
+    #10;
     rst =1;
     #10
     rst =0;
 
-    enb =1;
+    load =1;
+    for(integer i =0;i<=10;i=i+1)begin
+        din = i;#10;
+    end
     #100;
-    enb=0;
+    load=0;
    
     $finish;
 end
